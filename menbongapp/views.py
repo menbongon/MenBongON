e08372ac46081e1e011965153a36d7d7ae518337
Sujin <sujin.k0402@gmail.com>
2019-08-04 16:11:19 +0900

    메인화면 작성

--- menbongapp/views.py
+++ menbongapp/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 def intro(request):
     return render(request, 'intro.html')
 
-def home(request):
-    return render(request, 'home.html')
 
+def home(request):
+    username=request.user.username
+    password=request.user.password
+    return render(request, 'home.html',{'username':username,'password':password})
