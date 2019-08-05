e7ae74db6ef3b82727b85d565998bb1652548ef0
do02reen24 <do02reen24@gmail.com>
2019-08-05 12:39:25 +0900

    홍보게시판, Q&A게시판 모델 생성

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Promotion_post, QandA_post, Promotion_comment, QandA_comment
 
 # Register your models here.
+admin.site.register(Promotion_post)
+admin.site.register(QandA_post)
+admin.site.register(Promotion_comment)
+admin.site.register(QandA_comment)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+#게시판
+class Promotion_post(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField('date published')
+    event_date = models.DateTimeField('event day')
+    body = models.TextField()
+    
+    def publish(self):
+        self.pub_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class QandA_post(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+
+    def publish(self):
+        self.pub_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+#댓글
+class Promotion_comment(models.Model):
+    #writer = models.user.username
+    pub_date = models.DateTimeField('date published')
+    comment = models.TextField(max_length=500)
+
+    def publish(self):
+        self.pub_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.comment
+
+class QandA_comment(models.Model):
+    #writer = models.user.username
+    pub_date = models.DateTimeField('date published')
+    comment = models.TextField(max_length=500)
+
+    def publish(self):
+        self.pub_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.comment
