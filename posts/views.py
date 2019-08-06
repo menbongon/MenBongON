0ad7e88900b35eec7664e78028aebbe83b81df2a
do02reen24 <do02reen24@gmail.com>
2019-08-06 12:12:52 +0900

    홍보 및 Q&A 게시판 기본 url 셋팅

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Promotion_post, Promotion_comment, QandA_post, QandA_comment
 
 def notice(request):
     return render(request, 'noticeboard.html')
@@ -13,8 +14,16 @@ def promotionWrite(request):
     user_type=request.user.user_type
     if user_type == 0 or user_type == 1:
         return render(request, 'promotionwrite.html')
-    return redirect('promotion')
-    
+    return render(request, 'promotionwrite.html') #권한과 상관없이 이동가능하게 하였음. 추후 수정
+    #return redirect('promotion')
+
+def promotionCreate(request):
+    return render(request, 'promotionboard.html')
+
+def promotionDetail(request, promotion_id):
+    details = get_object_or_404(Promotion_post, pk = promotion_id)
+    return render(request, 'promotiondetail.html', {'details':details})
+
 def qna(request):
     return render(request, 'qnaboard.html')
 
@@ -22,7 +31,15 @@ def qnaWrite(request):
     user_type=request.user.user_type
     if user_type == 3:
         return render(request, 'qnawrite.html')
-    return redirect('qna')
+    return render(request, 'promotionwrite.html') #권한과 상관없이 이동가능하게 하였음. 추후 수정
+    #return redirect('qna')
+
+def qnaCreate(request):
+    return render(request, 'qnaboard.html')
+
+def qnaDetail(request, qna_id):
+    details = get_object_or_404(QandA_post, pk = qna_id)
+    return render(request, 'qnadetail.html', {'details':details})
 
 def review(request):
     return render(request, 'reviewboard.html')
