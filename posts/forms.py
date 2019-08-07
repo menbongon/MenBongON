8c29c0053c081e021b546bd7fa0ae8ef512ca381
do02reen24 <do02reen24@gmail.com>
2019-08-07 22:40:13 +0900

    refactoring Q&A and Promotion

--- posts/forms.py
+++ posts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import *
 
+# 공지게시판
 class NoticePostForm(forms.ModelForm):
     class Meta:
         model = Notice_post
@@ -12,7 +13,31 @@ class NoticeCommentForm(forms.ModelForm):
         model = Notice_comment
         fields = ['body']
 
+# 홍보게시판
+class PromotionPostForm(forms.ModelForm):
+    class Meta:
+        model = Notice_post
+        fields = ['title', 'body', 'image']
+    
 
+class PromotionCommentForm(forms.ModelForm):
+    class Meta:
+        model = Notice_comment
+        fields = ['body']
+
+# Q&A게시판
+class QnAPostForm(forms.ModelForm):
+    class Meta:
+        model = Notice_post
+        fields = ['title', 'body', 'image']
+    
+
+class QnACommentForm(forms.ModelForm):
+    class Meta:
+        model = Notice_comment
+        fields = ['body']
+
+# 일대일 게시판
 class OneononePostForm(forms.ModelForm):
     class Meta:
         model = Oneonone_post
