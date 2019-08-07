93dd1d9665e8da9cfc8b418a26e680dcc26b540e
do02reen24 <do02reen24@gmail.com>
2019-08-07 15:22:28 +0900

    모델 업데이트

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-#게시판
+User = get_user_model()
+
+#홍보 게시판 (작성자, 제목, 작성시간, 내용, 홍보포스터)
 class Promotion_post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=50)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
@@ -19,7 +24,9 @@ class Promotion_post(models.Model):
     def summary(self):
         return self.body[:100]
 
+#Q&A 게시판 (제목, 작성시간, 내용)
 class QandA_post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=50)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
@@ -34,12 +41,12 @@ class QandA_post(models.Model):
     def summary(self):
         return self.body[:100]
 
-#댓글
+# 홍보게시판 댓글 (작성자, 어떤 글, 작성시간, 내용)
 class Promotion_comment(models.Model):
-    #writer = models.user.username
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    post = models.ForeignKey(Promotion_post, on_delete=models.CASCADE, null=True, blank=True)
     pub_date = models.DateTimeField('date published')
     comment = models.TextField(max_length=500)
-    post_id = models.IntegerField(null=True)
 
     def publish(self):
         self.pub_date = timezone.now()
@@ -48,8 +55,10 @@ class Promotion_comment(models.Model):
     def __str__(self):
         return self.comment
 
+# Q&A 게시판 댓글 (작성자, 어떤 글, 작성시간, 내용)
 class QandA_comment(models.Model):
-    #writer = models.user.username
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    post = models.ForeignKey(QandA_post, on_delete=models.CASCADE, null=True, blank=True)
     pub_date = models.DateTimeField('date published')
     comment = models.TextField(max_length=500)
 
