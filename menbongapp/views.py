f76d634169d7bf34592a226ec4e4cc180f21008f
Sujin <sujin.k0402@gmail.com>
2019-09-18 03:00:12 +0900

    메인페이지 공지사항 및 홍보게시판 최근 5개씩 가져오기

--- menbongapp/views.py
+++ menbongapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from accounts.models import University
+from posts.models import *
 import logging
 
 def intro(request):
@@ -12,8 +13,10 @@ def home(request):
         password=request.user.password
     except:
         return render(request, 'intro.html')
-        
-    return render(request, 'home.html',{'username':username,'password':password})
+    recent_notice_posts = Notice_post.objects.all().order_by('-pub_date')[:5]
+    recent_promotion_posts = Promotion_post.objects.order_by('-pub_date').exclude(image__isnull=True)[:5]
+    return render(request, 'home.html',{'username':username,'password':password, 'recent_notice_posts':recent_notice_posts, 'recent_promotion_posts':recent_promotion_posts})
+
 
 def menbong(request):
     try:
